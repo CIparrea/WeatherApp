67ecf1f69a181b145c8365dc6c4002f11eb17301
Cesar Iparrea <cesariparrea@hotmail.com>
2024-02-06 12:41:25 -0500

    1

---
+++ js.js
@@ -0,0 +1,55 @@
+const apiKey = "d28537c1ffdbeb9ecb880a9d21ccb446";
+const form = document.querySelector("form");
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(e){
+    e.preventDefault();
+    const city = document.querySelector("input").value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+
+fetch(url)
+  .then((res) => res.json())
+  .then((res) => {
+    const name = document.querySelector(".name");
+    const description = document.querySelector(".description");
+    const tempC = document.querySelector(".tempC");
+    const tempF = document.querySelector(".tempF");
+    const humidity = document.querySelector(".humidity");
+    const wind = document.querySelector(".wind");
+ 
+    let tempFRounded = Math.round(((Number(res.main.temp))*(5/9))+32);
+    let tempCRounded = Math.round(res.main.temp)
+
+
+    let splitCity = city.split(" ");
+        let capSplitCity = splitCity.map(word => {
+            return word[0].toUpperCase() + word.slice(1);
+        })
+        let cityNew = capSplitCity.join(" ");
+
+
+    let condition = res.weather[0].description;
+    let splitCondition = condition.split(" ");
+        let capSplitCondition = splitCondition.map(word => {
+            return word[0].toUpperCase() + word.slice(1);
+        })
+        let conditionNew = capSplitCondition.join(" ");
+
+
+    name.textContent = cityNew;
+    description.textContent = conditionNew;
+    tempC.textContent = `${tempCRounded} °C`;
+    tempF.textContent = `${tempFRounded}`;
+    humidity.textContent = `Humidity: ${res.main.humidity} %`;
+    wind.textContent = `Wind: ${res.wind.speed} km/h`;
+
+    // console.log(`Temperature: ${res.main.temp} *C`);
+    // console.log(((Number(res.main.temp))*(5/9))+32);
+    // console.log(`Humidity: ${res.main.humidity} %`);
+    // console.log(`wind: ${res.wind.speed} km/h`);
+    // console.log(`img: ${res.weather[0].icon}`);
+    // console.log(res)
+  });
+
+}
